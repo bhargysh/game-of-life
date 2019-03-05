5b93bf5b1e511b15280641455575fe63a5ac8dd8
Bharg Sharma <bharg.sharma@rea-group.com>
2019-03-05 16:21:27 +1100

    Restructures the formatting of the grid

--- grid_logger.rb
+++ grid_logger.rb
@@ -4,16 +4,16 @@ class GridLogger
       columns = grid.y_range.size
       output << "+#{'-'* columns}+\n"
       grid.y_range.reverse_each do |y_cell|
-          grid.x_range.each do |x_cell|
+        output << "|"
+        grid.x_range.each do |x_cell|
             coordinate = Coordinate.new(x_cell, y_cell)
-            output << "|" unless grid.check?(coordinate.left)
             if grid.alive?(coordinate)
               output << "@"
             else
               output << "x"
             end
-            output << "|\n" unless grid.check?(coordinate.right)
-          end
+        end
+        output << "|\n"
       end
       output << "+#{'-'* columns}+\n"
       output
